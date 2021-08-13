717f646ed86adc24d874800cdd66179caa1b82fb
Stas Kukarkin <staskykarkin@gmail.com>
2021-08-13 21:38:31 +0600

    first commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app-header';
+
+
+const AppHeader = () => {
+    return(
+        <div className="app-header d-flex">
+        <h1>Stanislav</h1>
+        <h2>5 записей, из них понравилось 0</h2>
+        </div>
+    )
+
+}
+
+export default AppHeader;
